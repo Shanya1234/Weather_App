38c0dbaeea4fee15caf67d6bb6082530d4890bae
Shanya Jain <jainshanya77@gmail.com>
2024-10-25 16:05:29 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "d3fbd4a987mshbf149b607b8a476p148d01jsne53f0351201f",
+    "X-RapidAPI-Key": "",
     "X-RapidAPI-Host": "ip-geo-location.p.rapidapi.com",
   },
 };
@@ -9,4 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://ip-geo-location.p.rapidapi.com/ip/check?format=json&language=en";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "20e1e4768d74ff6a87c715d2cb748e98";
+export const WEATHER_API_KEY = "";
